2f3c41e9a6578aa36a06ce115c737b415288bf91
GiomarMoreno <giomarmoreno01@gmail.com>
2026-05-20 11:19:56 -0500

    commit Usuario.h inicial

---
+++ include/Usuario.h
@@ -0,0 +1,12 @@
+#ifndef USUARIO_H
+#define Usuario_h
+
+#include "Persona.h"
+/**
+ * @class clase Estudiante.
+ * @brief clase Estudiante heredada de Persona.
+ */
+class Usuario : public Persona{
+
+};
+#endif
