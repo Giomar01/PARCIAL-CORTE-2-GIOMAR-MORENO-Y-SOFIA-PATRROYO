29c44ce491d204578de98752993f1c343ad4c157
GiomarMoreno <giomarmoreno01@gmail.com>
2026-05-20 13:02:48 -0500

    commit usuario.cpp modified

--- src/Usuarios.cpp
+++ src/Usuarios.cpp
@@ -1,1 +1,22 @@
 #include "../include/Usuario.h"
+
+Usuario:: Usuario(std::string nombre, std::string rol, int documento, int edad, int codigoUsuario, int cantidadLibrosPrestados )
+    :Persona(nombre, rol, documento, edad ){
+        this->codigoUsuario=codigoUsuario;
+        this->cantidadLibrosPrestados=cantidadLibrosPrestados;
+    }
+     void Usuario::setCodigoUsuario(int codigoUsuario){
+        this->codigoUsuario=codigoUsuario;
+     }
+     void Usuario::setCantidadLibrosPrestados(int cantidadLibrosPrestados){
+        this->cantidadLibrosPrestados=cantidadLibrosPrestados;
+     }
+     int Usuario::getCodigoUsuario(){
+        return codigoUsuario;
+     }
+     int Usuario::getCantidadLibrosPrestados(){
+        return cantidadLibrosPrestados;
+     }
+     void Usuario::mostrarInformacion(){
+        
+     }
