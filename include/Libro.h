4f4d7beb4bd3c837203d71740c3d681e01ecd2fb
GiomarMoreno <giomarmoreno01@gmail.com>
2026-05-20 10:49:31 -0500

    commit Libro.h inicial

---
+++ include/Libro.h
@@ -0,0 +1,17 @@
+#ifndef LIBRO_H
+#define LIBRO_H
+
+
+#include<iostream>
+#include<string>
+
+/**
+ * @class Libro
+ * @brief Clase que representa un libro en la biblioteca 
+ 
+ */
+class Libro{
+    
+
+};
+#endif
