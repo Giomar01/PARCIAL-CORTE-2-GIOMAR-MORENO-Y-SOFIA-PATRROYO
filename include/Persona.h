b7ae841def6f7bbc409b65ff25c12e819d05b2d3
GiomarMoreno <giomarmoreno01@gmail.com>
2026-05-20 10:46:49 -0500

    commit Clase Persona Inicial.h

---
+++ include/Persona.h
@@ -0,0 +1,15 @@
+#ifndef PERSONA_H
+#define PERSONA_H
+
+#include <iostream>
+#include <string>
+
+
+/**
+ * @class Persona
+ * @brief clase Persona abstracta del sistema.
+ */
+class Persona{
+
+};
+#endif
