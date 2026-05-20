7fd0e6aea135e34e846d2d3eb6cd7c897593e2ce
GiomarMoreno <giomarmoreno01@gmail.com>
2026-05-20 10:56:42 -0500

    commit atributos y constructor clase persona

--- include/Persona.h
+++ include/Persona.h
@@ -10,6 +10,24 @@
  * @brief clase Persona abstracta del sistema.
  */
 class Persona{
+    private:
+    std::string nombre, rol;
+    int documento, edad;
+
+    public:
+
+    /**
+     * @brief Constructor de la clase Persona.
+     * @param nombre Nombre de la persona.
+     * @param rol cargo o rol que desempeña el sujeto.
+     * @param documento documento de la persona.
+     * @param edad Edad de la persona.
+     */
+    Persona(std::string nombre, std::string rol, int documento, int edad );
+
+    virtual ~Persona();
+
+    
 
 };
 #endif
