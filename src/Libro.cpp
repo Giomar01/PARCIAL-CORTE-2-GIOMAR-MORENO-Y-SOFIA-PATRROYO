321dd36142e9c382bef1a5e1bc7a070f0347bce6
GiomarMoreno <giomarmoreno01@gmail.com>
2026-05-20 13:58:31 -0500

    commit Libro.cpp final

--- src/Libro.cpp
+++ src/Libro.cpp
@@ -23,4 +23,57 @@ Libro::Libro(std:: string titulo,std:: string autor,std::string disponible,int c
     std::cout << "Disponibilidad: "
               << disponible << std::endl;
     }
-    
+    void Libro::prestarLibro(){
+
+    if(disponible == "Disponible"){
+
+        disponible = "No disponible";
+
+        std::cout << "Libro prestado correctamente"<< std::endl;
+
+    }else{
+
+        std::cout << "El libro no esta disponible" << std::endl;
+    }
+}
+
+void Libro::devolverLibro(){
+
+    disponible = "Disponible";
+
+    std::cout << "Libro devuelto correctamente" << std::endl;
+}
+
+// Getters
+
+int Libro::getCodigoLibro(){
+
+    return codigoLibro;
+}
+
+std::string Libro::getDisponible(){
+
+    return disponible;
+}
+
+// Setters
+
+void Libro::setTitulo(std::string titulo){
+
+    this->titulo = titulo;
+}
+
+void Libro::setAutor(std::string autor){
+
+    this->autor = autor;
+}
+
+void Libro::setDisponibles(std::string disponible){
+
+    this->disponible = disponible;
+}
+
+void Libro::setCodigoLibro(int codigoLibro){
+
+    this->codigoLibro = codigoLibro;
+}
