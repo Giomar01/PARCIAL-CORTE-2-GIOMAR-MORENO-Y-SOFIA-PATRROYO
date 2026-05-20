7bedb5a4b1645cac53a05068c0b7351966a13b77
GiomarMoreno <giomarmoreno01@gmail.com>
2026-05-20 14:26:08 -0500

    commit Bibliotecarios.cpp final

--- src/Bibliotecarios.cpp
+++ src/Bibliotecarios.cpp
@@ -6,3 +6,27 @@ Bibliotecario::Bibliotecario(std::string nombre, int documento, int edad, int co
 
     this->codigoEmpleado = codigoEmpleado;
     this->salario = salario;
+}
+    void Bibliotecario::mostrarInformacion(){
+     std::cout << "Nombre: " << getNombre() << std::endl;
+     std::cout << "Documento: " << getDocumento() << std::endl;
+     std::cout << "Edad: " << getEdad() << std::endl;
+     std::cout << "Codigo Empleado: " << codigoEmpleado << std::endl;
+     std::cout << "Salario: " << salario << std::endl;
+     mostrarRol();
+    }
+    void Bibliotecario::mostrarRol(){
+      std::cout << "Rol: Bibliotecario"<< std::endl;
+    }
+    int Bibliotecario::getCodigoEmpleado(){
+      return codigoEmpleado;
+    }
+    int Bibliotecario::getSalario(){
+      return salario;
+    }
+    void Bibliotecario::setCodigoEmpleado(int codigoEmpleado){
+      this->codigoEmpleado = codigoEmpleado;
+    }
+    void Bibliotecario::setSalario(int salario){
+      this->salario = salario;
+    }
