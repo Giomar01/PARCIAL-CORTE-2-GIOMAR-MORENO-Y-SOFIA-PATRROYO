d1e7f5d722685d7d6ab1b74274e0ea1eb33d08cc
GiomarMoreno <giomarmoreno01@gmail.com>
2026-05-20 14:22:24 -0500

    commit main.cpp declaracion variables

--- main/main.cpp
+++ main/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <vector>
 #include "../include/Usuario.h"
 #include "../include/Bibliotecarios.h"
 #include "../include/Libro.h"
 
 int main(){
     int opcion;
+    std::vector<Usuario> usuarios;
+    std::vector<Bibliotecario> bibliotecarios;
+    std::vector<Libro> libros;
+
 }
