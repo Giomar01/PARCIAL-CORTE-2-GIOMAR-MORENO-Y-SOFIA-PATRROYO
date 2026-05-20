0f7f4b3b534091bfd5be3574b4020fc2a38b965c
GiomarMoreno <giomarmoreno01@gmail.com>
2026-05-20 11:52:51 -0500

    commi Bibliotecarios final

--- include/Bibliotecarios.h
+++ include/Bibliotecarios.h
@@ -12,5 +12,43 @@ class Bibliotecario : public Persona{
 private:
  int codigoEmpleado;
  int salario;
+ public:
+ /**
+  * @brief Constructor de clase persona 
+  * @param nombre del biblotecario
+  * @param documento del bibliotecario
+  * @param edad del bibliotecario 
+  * @param codigoEmpleado codigo del empleado
+  * @param salario salrio del empleado 
+  * 
+  */
+ Bibliotecario(std::string nombre,int documento,int edad,int codigoEmpleado,int salario );
+
+
+ /**
+  * @brief muestra la informacion y el rol de la persona 
+  * 
+  */ 
+ void mostrarInformacion()override;
+ 
+
+ //Getters 
+
+ /**
+  * @brief Obtiene el codigo del empleado
+  * @return int
+  * 
+  */
+  int getCodigoEmpleado();
+  int getSalario();
+
+  //Setters 
+
+  /**
+   * @brief Modifica el codigo del empleado y el salario 
+   * @param CodigoEmpleado,salario
+   */
+  void setCodigoEmpleado (int codigoEmpleado);
+  void setSalario(int salario);
 };
 #endif
