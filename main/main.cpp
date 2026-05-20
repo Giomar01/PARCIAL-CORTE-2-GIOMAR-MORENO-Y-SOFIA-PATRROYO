eab777d9f59ebb7fd0fbf8789d9d2a68b993e00b
GiomarMoreno <giomarmoreno01@gmail.com>
2026-05-20 14:34:18 -0500

    commit main.cpp declaracion menu

--- main/main.cpp
+++ main/main.cpp
@@ -10,4 +10,24 @@ int main(){
     std::vector<Bibliotecario> bibliotecarios;
     std::vector<Libro> libros;
 
+
+    do{
+        std::cout << std::endl;
+        std::cout << "========= BIBLIOTECA =========" << std::endl;
+        std::cout << "1. Registrar usuario" << std::endl;
+        std::cout << "2. Registrar bibliotecario" << std::endl;
+        std::cout << "3. Registrar libro" << std::endl;
+        std::cout << "4. Prestar libro" << std::endl;
+        std::cout << "5. Devolver libro" << std::endl;
+        std::cout << "6. Mostrar usuarios" << std::endl;
+        std::cout << "7. Mostrar libros" << std::endl;
+        std::cout << "8. Salir" << std::endl;
+        std::cout << "Opcion: ";
+        std::cin >> opcion;
+
+        switch(opcion){
+
+    }while();
+
+    return 0;
 }
